a330cc2bb30ecb5de8207c6d38a6d9f53f9a7a95
Muhammad Gad <mohamad.gad16@gmail.com>
2021-08-24 18:56:56 +0200

    list trending languages and thier repos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from datetime import datetime, timedelta
-import requests
+import requests, json
 
 app = Flask(__name__)
 
@@ -21,7 +21,25 @@ def list_of_lang():
     this function will return an object contains on the list of languages
     and number and list of repositories for each language.
     '''
-    pass
+    repos = get_repos()
+    languages = {}
+    for repo in repos['items']:
+        lang = repo['language']
+        if lang != None:
+            if lang in languages :
+                languages[lang]["repositories_count"] +=1
+                languages[lang]["repositories"].append({'repository_name': repo['name'],
+                                                        'repository_url': repo['html_url']
+                                                        })
+            else:
+                languages[lang] = {}
+                languages[lang]["repositories_count"] = 1
+                languages[lang]["repositories"] = []
+                languages[lang]["repositories"].append({'repository_name': repo['name'],
+                                                        'repository_url': repo['html_url']
+                                                        })
+    languages = json.dumps(languages)
+    return languages
 
 @app.route('/trending_languages', methods=['GET'])
 def get_lang():
@@ -29,7 +47,7 @@ def get_lang():
     this is an endpoint to return the list of trending languages that
     including the count and list of repositories for each language.
     '''
-    pass
+    return list_of_lang()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
